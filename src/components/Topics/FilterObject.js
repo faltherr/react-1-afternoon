e1122306149992e67a4f944d48a70804837f1a2a
L245854 <l245854@pn1706924.lanl.gov>
2018-07-13 16:13:40 -0600

    Initial commit for the JS3 project

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,62 @@
+import React , {Component} from 'react';
+
+class FilterObject extends Component{
+    constructor(){
+        super ();
+            this.state={
+                unFilteredArray :[
+                    {
+                      name: 'Jimmy Joe',
+                      title: 'Hack0r',
+                      age: 12,
+                    },
+                    {
+                      name: 'Jeremy Schrader',
+                      age: 24,
+                      hairColor: 'brown'
+                    },
+                    {
+                      name: 'Carly Armstrong',
+                      title: 'CEO',
+                    }
+                  ],
+                userInput : '',
+                filteredArray : []
+            }
+        this.handleChange = this.handleChange.bind(this)
+        this.clicker = this.clicker.bind(this)
+    }
+
+    //On change method
+    handleChange(val){
+        this.setState({userInput:val})
+    }
+
+    clicker(prop){
+        var unFilter = this.state.unFilteredArray;
+        var filteredArray = [];
+        for (var i= 0; i<unFilter.length;i++){
+            if (unFilter[i].hasOwnProperty(prop)){
+                filteredArray.push(unFilter[i])
+            }
+        }
+        this.setState({filteredArray : filteredArray})//Not functioning? check out protocol determining this usability
+
+    }
+
+
+
+    render(){
+        return(
+            <div className="puzzleBox filterObjectPB">
+            <h4>Filter Element</h4>
+                <span className="puzzleText"> Original Array: {JSON.stringify(this.state.unFilteredArray, null, 10)} </span>
+                <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+                <button className="confirmationButton" onClick={ () => { this.clicker(this.state.userInput) }}> Filter </button>
+                <span className="resultsBox filterObjectRB"> New Array: {JSON.stringify(this.state.filteredArray, null, 10)} </span>
+        </div>
+        )
+    }
+}
+
+export default FilterObject;
